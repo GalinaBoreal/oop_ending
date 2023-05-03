740e19c89bfe3407b65f6059573947dd5612b9fb
Galina Ivanova <galina_ver@mail.ru>
2023-05-03 12:59:07 +0500

    fixed project

--- vk_yd.py
+++ vk_yd.py
@@ -1,54 +1,41 @@
-import requests
-import yadisk
-import wget
 import json
 import os
-from datetime import datetime
+import requests
 from tqdm import tqdm
+from dotenv import load_dotenv
+from yd import YD
 
 
 class VK:
 
-    def __init__(self, ja_token, album_id, user_id, count=5, version='5.131'):
-        self.access_token = "vk1.a.puVHDA5Y3YK8xSN383DaNHdHbGswjNWq6qvTGlk0T4K0ZT3T" \
-                            "-zpi6H5dIy4PtmTPOCm8Q9dVNhJO34MXfqikTtFqWefxoMnz2oeBrQpn9UAr" \
-                            "-m7vPblRtlmPRbTFKdOLBkY3TaFWnLF9OpSUr_BxDyxAyYew7soi_uhU2YGe" \
-                            "-NqUHm4c4O8X9HOXl0Ds8E90NHDdFsC_r0eOpcXuKVfOwA"
-        self.ya_token = ya_token
+    def __init__(self, access_token, album_id, user_id, count=5, version='5.131'):
+        self.access_token = access_token
         self.id = user_id
         self.album = album_id
         self.count = count
         self.version = version
         self.params = {'access_token': self.access_token, 'v': self.version}
-        self.get_photo_data = {}
-        self.photo_name_list = []
-        self.persona_name = []
 
     def _users_info(self):
         """
-        Получение информации о пользователе
-        param: user_id
-        param: params
+        Получает в ВК информацию о пользователе методом get.
+        :param: user_id, params
+        :return: response.json() или сообщение об ошибке и завершение программы
         """
         url = 'https://api.vk.com/method/users.get'
         params = {'user_ids': self.id}
         response = requests.get(url=url, params={**self.params, **params})
-        if response.status_code == 200:
-            response = response.json()
-            if response['response'] != []:
-                self.persona_name.append(response['response'][0]['first_name'])
-                self.persona_name.append(response['response'][0]['last_name'])
+        if response.status_code < 200 and response.status_code > 300:
+            print(f'Ответ VK: ошибка {response.status_code}')
+            return exit(0)
         else:
-            return 'Что то пошло не так при получении информации о профиле VK.'
-        return
+            return response.json()
 
     def _get_photo_data(self):
         """
-        Запросить данные для загрузки фотографий методом photos.get,
-        получить итоговые данные в формате json,
-        param: album_id
-        param: count
-        param: params
+        Запрашивает данные для загрузки фотографий методом photos.get
+        :param: album_id, user_id, count, params
+        :return: response.json() или сообщение об ошибке и завершение программы
         """
         url = "https://api.vk.com/method/photos.get"
         params = {
@@ -60,109 +47,88 @@ class VK:
             'count': self.count
         }
         response = requests.get(url=url, params={**self.params, **params})
-        if response.status_code == 200:
-            response = response.json()
-            self.get_photo_data.update(response)
-            return self.get_photo_data
+        if response.status_code < 200 and response.status_code > 300:
+            print(f'Ответ VK: ошибка {response.status_code}')
+            return exit(0)
         else:
-            return 'Что то пошло не так при получении информации о фотографиях VK.'
+            return response.json()
 
-    def _download_photo(self):
+    def _get_photo_url(self):
         """
-        Загружает фото в локальную папку Photos,
-        присвоив файлу имя = количество лайков или
-        дата+количество лайков, если лайков одинаковое
-        количество.
-        Запись результата загрузки в файл photos.json
-        params: self._get_photo_data
+        Вызывает метод _get_photo_data() и обрабатывает ответ,
+        выбирая наибольшие фотографии ВК. Имена фотографий = количество лайков
+        или дата + количество лайков, если лайков одинаковое количество.
+        Формирует файл photos.json.
+        :return: словарь name_url, ключ - имя файла, значение - ссылка для загрузки.
         """
-
-        photo_list = []
+        json_list = []
+        name_url = {}
         type_list = ['w', 'z', 'y', 'r', 'q', 'p', 'o', 'x', 'm', 's']
-
-        for item in self.get_photo_data['response']['items']:
+        data = self._get_photo_data()
+        for item in data['response']['items']:
             flag = False
             photo_name = str(item['likes']['count']) + '.jpg'
-            if photo_name not in self.photo_name_list:
-                self.photo_name_list.append(photo_name)
+            if photo_name not in name_url:
+                name_url[photo_name] = None
             else:
                 photo_name = str(item['date']) + photo_name
-                self.photo_name_list.append(photo_name)
+                name_url[photo_name] = None
             for type in type_list:
                 for dict in item['sizes']:
                     if dict['type'] == type and flag == False:
                         flag = True
                         url = dict['url']
                         size = dict['type']
-                        wget.download(url=url, out='Photos/' + photo_name)
-                        photo_list.append({'file_name': photo_name, 'size': size})
+                        name_url[photo_name] = [url]
+                        json_list.append({'file_name': photo_name, 'size': size})
 
         with open('photos.json', 'w') as outfile:
-            json.dump(photo_list, outfile)
+            json.dump(json_list, outfile)
 
-        return self.photo_name_list
+        return name_url
 
-    def _upload_poto_yd(self):
+    def uploader(self):
         """
-        Вызывает self._download_photo() для загрузки файлов в Photos.
-        Создает папку с текущей датой на яндекс диске
-        и, используя библиотеку-клиент REST API Яндекс.Диска jadisk,
-        загружает в нее фотографии из локальной папки Photos,
-        param: self._download_photo()
-        param: ya_token
-        param: name_list
+        Загрузка фотографий на яндекс диск.
+        Проверяет ID пользователя: не существует, закрыт, нет фото - выводит сообщение.
+        Вызывает self._users_info() для получения информации о пользователе.
+        Вызывает self._get_photo_url() для получения данных для загрузки.
+        Вызывает класс YD: yd.get_folder() для создания папки,
+        имя папки: Photo_VK_ + user_id; yd.get_upload_link() для загрузки
+        файлов, процесс загрузки сопровождается прогресс баром tqdm.
+        :return: Завершение программы
         """
-        y = yadisk.YaDisk(token=self.ya_token)
-        flag = y.check_token()
-        date = str(datetime.now().date())
-        dir = 'Photo_VK_' + date
-        if flag == False:
-            print("Неверный токен яндекс диска.")
+        info = self._users_info()
+        if info['response'] == []:
+            return f'ID не существует.'
         else:
-            try:
-                y.mkdir(dir)
-                for i in tqdm(range(len(self.photo_name_list))):
-                    name = self.photo_name_list[i]
-                    y.upload(f'Photos/{name}', f'{dir}/{name}')
-                print(
-                    f'Фотографии {self.album} пользователя {" ".join(self.persona_name)} загружены на яндекс диск '
-                    f'успешно.')
-            except Exception:
-                print('Что то пошло не так во время загрузки фотографий на яндекс диск.')
-        return
-
-    def check_profile(self):
-        """
-        Проверка профиля пользователя по ключевым моментам:
-        существует ли Id, не закрыт ли профиль, есть ли фото.
-        Если исключений нет, вызывает метод self._upload_poto_yd()
-        для загрузки и выгрузки файлов и создания итогового файла photos.json
-        """
-        self._users_info()
-        if self.persona_name == []:
-            return 'Id не существует.'
-        else:
-            self._get_photo_data()
-            if 'error' in self.get_photo_data:
-                return f'Профиль пользователя {" ".join(self.persona_name)} закрыт, либо неверно введен альбом.'
+            data = self._get_photo_data()
+            if 'error' in data:
+                return f'У пользователя {info["response"][0]["first_name"]} \
+{info["response"][0]["last_name"]} закрытый профиль.'
             else:
-                if self.get_photo_data['response']['items'] == []:
-                    return f'У пользователя {" ".join(self.persona_name)} нет фотографий.'
+                if data['response']['items'] == []:
+                    return f'У пользователя {info["response"][0]["first_name"]} \
+{info["response"][0]["last_name"]} нет фотографий'
                 else:
-                    self._download_photo()
-                    print(f'\nФотографии {self.album} пользователя {" ".join(self.persona_name)} скопированы с VK '
-                          f'успешно.\nЗагружаем на яндекс диск...')
-                    self._upload_poto_yd()
-                    for name in self.photo_name_list:
-                        path = "Photos/" + name
-                        os.remove(path)
-                    return '\nПрограмма завершена!'
+                    dict = self._get_photo_url()
+                    dir = 'Photo_VK_' + str(self.id) + '/'
+                    yd.get_folder(ydisk_folder_path=dir)
+                    for key in tqdm(dict.keys()):
+                        path = dir + str(key)
+                        yd.get_upload_link(path, dict[key])
+        return f'Фотографии {self.album} пользователя {info["response"][0]["first_name"]} \
+{info["response"][0]["last_name"]} загружены на яндекс диск успешно!'
 
+
+load_dotenv()
+access_token = os.getenv('ACCESS_TOKEN')
 
 ya_token = str(input('Введите токен яндекс диска: '))
 album_id = str(input('Для выбора альбома введите profile или wall: '))
 album_id = album_id.lower()
 user_id = str(input('Введите id пользователя VK: '))
 
-vk = VK(ya_token, album_id, user_id)
-print(vk.check_profile())
+yd = YD(token=ya_token)
+vk = VK(access_token, album_id, user_id)
+print(vk.uploader())
---
+++ yd.py
@@ -0,0 +1,41 @@
+import requests
+
+class YD:
+
+    def __init__(self, token):
+        self.token = token
+
+    def get_folder(self, ydisk_folder_path):
+        """
+        Создает папку на яндекс диске.
+        :param ydisk_folder_path: расположение и имя папки
+        :return: в случае ошибки - код ошибки
+        """
+        url = "https://cloud-api.yandex.net/v1/disk/resources"
+        headers = {
+            'Content-Type': 'application/json',
+            'Authorization': f'OAuth {self.token}'
+        }
+        params = {"path": ydisk_folder_path}
+        response = requests.put(url=url, headers=headers, params=params)
+        if response.status_code < 200 and response.status_code > 300:
+            return f'Ошибка YD {response.status_code}'
+
+
+    def get_upload_link(self, ydisk_file_path, url):
+        """
+        Загружает файл на яндекс диск по ссылке.
+        :param ydisk_file_path: расположение и имя файла на яндекс диске
+        :param url: ссылка на файл
+        :return: в случае ошибки - код ошибки
+        """
+        upload_url = "https://cloud-api.yandex.net/v1/disk/resources/upload"
+        headers = {
+            'Content-Type': 'application/json',
+            'Authorization': f'OAuth {self.token}'
+        }
+        params = {"path": ydisk_file_path, 'url': url}
+        response = requests.post(url=upload_url, headers=headers, params=params)
+        if response.status_code < 200 and response.status_code > 300:
+            return f'Ошибка YD {response.status_code}'
+
