3c67f15aa6ba06789cbca852f1fc7a8753cc873f
Galina Ivanova <galina_ver@mail.ru>
2023-05-04 20:00:44 +0500

    final fixed project

--- yd.py
+++ yd.py
@@ -1,5 +1,6 @@
 import requests
-
+from tqdm import tqdm
+import json
 
 class YD:
 
@@ -19,23 +20,33 @@ class YD:
         }
         params = {"path": ydisk_folder_path}
         response = requests.put(url=url, headers=headers, params=params)
-        if response.status_code != 201:
+        if response.status_code not in (201, 409):
             exit(f'Ошибка YD {response.status_code}')
 
-    def upload_photo(self, ydisk_file_path, url):
+    def upload_photo(self, data, ydisk_folder_path="Photos_vk"):
         """
         Загружает файл на яндекс диск по ссылке.
         :param ydisk_file_path: расположение и имя файла на яндекс диске
         :param url: ссылка на файл
         :return: в случае ошибки - код ошибки
         """
+        self.get_folder(ydisk_folder_path)
         upload_url = "https://cloud-api.yandex.net/v1/disk/resources/upload"
         headers = {
             'Content-Type': 'application/json',
             'Authorization': f'OAuth {self.token}'
         }
-        params = {"path": ydisk_file_path, 'url': url}
-        response = requests.post(url=upload_url, headers=headers, params=params)
-        if response.status_code != 202:
-            exit(f'Ошибка YD {response.status_code}')
-        return response.status_code
+        json_list = []
+        for key, value in tqdm(data.items()):
+            url = value['url']
+            file_name = key + '.jpg'
+            path = ydisk_folder_path + '/' + file_name
+            params = {"path": path, 'url': url}
+            response = requests.post(url=upload_url, headers=headers, params=params)
+            if response.status_code != 202:
+                exit(f'Ошибка YD {response.status_code}')
+            json_list.append({'file_name': file_name, 'size': value['size']})
+        with open('photos.json', 'w') as outfile:
+            json.dump(json_list, outfile)
+
+        return f'Фотографии загружены на яндекс диск успешно!'
